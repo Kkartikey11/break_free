9e2115a4e55b6e73afe554abfd574ae8e40e3440
kartikey-kumar <k2kartikey.pathak@gmail.com>
2023-04-14 01:34:38 +0530

    add batches update and delete API

--- controler/v1/batches/batches.js
+++ controler/v1/batches/batches.js
@@ -20,11 +20,59 @@ exports.getBatch = async (req, res, next) => {
     }
 };
 
+// delete batch by id
+exports.postDeleteBatch = async (req, res, next) => {
+    try {
+        let batchId = req.params.batchId;
+
+        let isdelete = await deleteBatch(batchId);
+        if (isdelete) {
+            return res.status(200).send({ code: 200, message: "Batch deleted successfully." })
+        }
+
+        return res.status(200).send({ code: 400, message: "Batch failed to delete." })
+
+    } catch (error) {
+        console.log("Error: ", error.message);
+        return res.status(200).send({ code: 400, message: error.message });
+    }
+};
+
 // return batches list
 exports.getBatches = async (req, res, next) => { 
     try {
         let batches = await getBatches();
         return res.status(200).send({ code: 200, data: batches });
+
+    } catch (error) {
+        console.log("Error: ", error.message);
+        return res.status(200).send({ code: 400, message: error.message });
+    }
+};
+
+exports.updateBatch = async (req, res, next) => { 
+    try {
+        let batchId = req.params.batchId;
+
+        let isBatch = await getBatch(batchId);
+        if (!isBatch || !isBatch.id) return res.send({ code: 400, message: "Please provide valid batch id." });
+
+        let query = req.body;
+        let name = query.name;
+        let description = query.description ? query.description : "";
+        let subject_id = query.subject_id;
+        let added_students = query.added_students;
+        let deleted_students = query.deleted_students;
+
+        if ((!name || name == '') & (!subject_id || subject_id == '') & ((!added_students || added_students.length == 0) & (!deleted_students || deleted_students.length == 0))) return res.send({ code: 400, message: "Please provide required details." });
+
+        let isUpdated = await updateBatch(batchId, name, description, subject_id);
+        if (isBatch) {
+            await updateBatchStudentsMapping(batchId, added_students, deleted_students);
+            return res.status(200).send({ code: 200, message: "Batch updated successfully." })
+        }
+
+        return res.status(200).send({ code: 400, message: "Batch failed to update." })
 
     } catch (error) {
         console.log("Error: ", error.message);
@@ -88,8 +136,59 @@ const savebatchStudentMapping = async (students, batch_id) => {
 };
 
 const getStudents = async (batchId) => { 
-    let query= `select s.id,s.name from batch_students_mapping as bm left join students as s on bm.student_id=s.id where bm.batch_id=${batchId}`;
+    let query= `select s.id,s.name from batch_students_mapping as bm left join students as s on bm.student_id=s.id where bm.batch_id=${batchId} and bm.is_deleted=0`;
     con.query = await util.promisify(con.query);
     let result = await con.query(query);
     return result
+};
+
+const updateBatch = async (batchId, name, description, subject_id) => { 
+    try {
+        let set = [];
+        if (name && name != '') set.push(`name='${name}'`);
+        if (description && description != '') set.push(`description='${description}'`);
+        if (subject_id && subject_id != '') set.push(`subject_id=${subject_id}`);
+    
+        let query = `update batches set ${set.join()} where id=${batchId}`;
+        con.query = await util.promisify(con.query);
+        let result = await con.query(query);
+        return true
+    } catch (error) {
+        console.log("Error: ", error);
+        return false;
+    }
+    
+
+};
+
+const updateBatchStudentsMapping = async (batchId, added_students, deleted_students) => {
+    if (added_students && added_students.length > 0) {
+        con.query = await util.promisify(con.query);
+        for (let index = 0; index < added_students.length; index++) {
+            const student_id = added_students[index];
+            let isMappedQuery = `select id from batch_students_mapping where batch_id=${batchId} and student_id=${student_id} and is_deleted=0`
+            let isStudentMapped = await con.query(isMappedQuery); 
+            if (isStudentMapped.length === 0) {
+                let query = `insert into batch_students_mapping (batch_id,student_id) values (${batchId}, ${student_id})`;
+                let result = await con.query(query); 
+            }
+            
+        }
+    }
+    if (deleted_students && deleted_students.length > 0) {
+        for (let index = 0; index < deleted_students.length; index++) {
+            const student_id = deleted_students[index];
+            let query = `update batch_students_mapping set is_deleted=1 where batch_id=${batchId} and student_id=${student_id}`;
+            con.query = await util.promisify(con.query);
+            let result = await con.query(query);
+        }
+    }
+
+};
+
+const deleteBatch = async (batchId) => {
+    let query = `update batches set is_deleted=1 where id=${batchId} and is_deleted=0`;
+    con.query = util.promisify(con.query);
+    let result = await con.query(query);
+    return result.affectedRows;
 };
